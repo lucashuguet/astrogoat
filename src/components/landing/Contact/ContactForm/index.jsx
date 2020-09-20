db2e27cc0a2fa79f8a5d6f835cfd2878842f902b
AstroGoat <astrogoat@localhost.localdomain>
2020-09-20 18:31:51 +0200

    Change design and add contact form working

--- src/components/landing/Contact/ContactForm/index.jsx
+++ src/components/landing/Contact/ContactForm/index.jsx
@@ -1,82 +1,33 @@
 import React from 'react';
 import axios from 'axios';
-import { Formik, Form, FastField, ErrorMessage } from 'formik';
-import Recaptcha from 'react-google-recaptcha';
-import * as Yup from 'yup';
+// import { Formik, Form, FastField, ErrorMessage } from 'formik';
+// import Recaptcha from 'react-google-recaptcha';
+// import * as Yup from 'yup';
 import { Button, Input } from 'components/common';
 import { Error, Center, InputField } from './styles';
 
-export default () => (
-  <Formik
-    initialValues={{
-      name: '',
-      email: '',
-      message: '',
-      recaptcha: '',
-      success: false,
-    }}
-    validationSchema={Yup.object().shape({
-      name: Yup.string().required('Full name field is required'),
-      email: Yup.string()
-        .email('Invalid email')
-        .required('Email field is required'),
-      message: Yup.string().required('Message field is required'),
-      recaptcha: Yup.string().required('Robots are not welcome yet!'),
-    })}
-    onSubmit={async ({ name, email, message }, { setSubmitting, resetForm, setFieldValue }) => {
-      try {
-        await axios({
-          method: 'POST',
-          url: `${process.env.GATSBY_PORTFOLIO_FORMIK_ENDPOINT}`,
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          data: JSON.stringify({
-            name,
-            email,
-            message,
-          }),
-        });
-        setSubmitting(false);
-        setFieldValue('success', true);
-        setTimeout(() => resetForm(), 6000);
-      } catch (err) {
-        setSubmitting(false);
-        setFieldValue('success', false);
-				alert('Something went wrong, please try again!') // eslint-disable-line
-      }
-    }}
-  >
-    {({ values, touched, errors, setFieldValue, isSubmitting }) => (
-      <Form>
-        <InputField>
+export default () => {
+    return (
+        <form name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
           <Input
-            as={FastField}
             type="text"
             name="name"
             component="input"
             aria-label="name"
-            placeholder="Full name*"
-            error={touched.name && errors.name}
-          />
-          <ErrorMessage component={Error} name="name" />
-        </InputField>
-        <InputField>
+            placeholder="Nom*"
+            required
+          /><br/><br/>
           <Input
             id="email"
             aria-label="email"
             component="input"
-            as={FastField}
             type="email"
             name="email"
             placeholder="Email*"
-            error={touched.email && errors.email}
-          />
-          <ErrorMessage component={Error} name="email" />
-        </InputField>
-        <InputField>
+            required
+          /><br/><br/>
           <Input
-            as={FastField}
+            as="textarea"
             component="textarea"
             aria-label="message"
             id="message"
@@ -84,34 +35,14 @@ export default () => (
             type="text"
             name="message"
             placeholder="Message*"
-            error={touched.message && errors.message}
-          />
-          <ErrorMessage component={Error} name="message" />
-        </InputField>
-        {values.name && values.email && values.message && (
-          <InputField>
-            <FastField
-              component={Recaptcha}
-              sitekey={process.env.GATSBY_PORTFOLIO_RECAPTCHA_KEY}
-              name="recaptcha"
-              onChange={value => setFieldValue('recaptcha', value)}
-            />
-            <ErrorMessage component={Error} name="recaptcha" />
-          </InputField>
-        )}
-        {values.success && (
-          <InputField>
-            <Center>
-              <h4>Your message has been successfully sent, I will get back to you ASAP!</h4>
-            </Center>
-          </InputField>
-        )}
-        <Center>
-          <Button secondary type="submit" disabled={isSubmitting}>
-            Envoyer
-          </Button>
-        </Center>
-      </Form>
-    )}
-  </Formik>
-);
+            style={{resize: "none"}}
+            required
+          /><br/><br/>
+          <Center>
+            <Button secondary type="submit" id="submit">
+              Envoyer
+            </Button>
+          </Center>
+        </form>
+    );
+};
