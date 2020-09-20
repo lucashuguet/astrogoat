287ae31a8ea835972aa618c0cb68fcca3dc49bbf
AstroGoat <astrogoat@localhost.localdomain>
2020-09-20 19:21:35 +0200

    Fix contact form

--- src/components/landing/Contact/ContactForm/index.jsx
+++ src/components/landing/Contact/ContactForm/index.jsx
@@ -1,48 +1,43 @@
 import React from 'react';
-import axios from 'axios';
-// import { Formik, Form, FastField, ErrorMessage } from 'formik';
-// import Recaptcha from 'react-google-recaptcha';
-// import * as Yup from 'yup';
 import { Button, Input } from 'components/common';
-import { Error, Center, InputField } from './styles';
+import { Center, InputField } from './styles';
 
-export default () => {
-    return (
-        <form name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
-          <Input
-            type="text"
-            name="name"
-            component="input"
-            aria-label="name"
-            placeholder="Nom*"
-            required
-          /><br/><br/>
-          <Input
-            id="email"
-            aria-label="email"
-            component="input"
-            type="email"
-            name="email"
-            placeholder="Email*"
-            required
-          /><br/><br/>
-          <Input
-            as="textarea"
-            component="textarea"
-            aria-label="message"
-            id="message"
-            rows="8"
-            type="text"
-            name="message"
-            placeholder="Message*"
-            style={{resize: "none"}}
-            required
-          /><br/><br/>
-          <Center>
-            <Button secondary type="submit" id="submit">
-              Envoyer
-            </Button>
-          </Center>
-        </form>
-    );
-};
+export default () => (
+    <form name="contact" netlify>
+      <Input
+        id="name"
+        type="text"
+        name="name"
+        component="input"
+        aria-label="name"
+        placeholder="Nom*"
+        required
+      /><br/>
+      <Input
+        id="email"
+        aria-label="email"
+        component="input"
+        type="email"
+        name="email"
+        placeholder="Email*"
+        required
+      /><br/>
+      <Input
+        as="textarea"
+        component="textarea"
+        aria-label="message"
+        id="message"
+        rows="8"
+        type="text"
+        name="message"
+        placeholder="Message*"
+        style={{resize: "none"}}
+        required
+      /><br/>
+      <Center>
+        <Button secondary type="submit">
+          Envoyer
+        </Button>
+      </Center>
+    </form>
+);
